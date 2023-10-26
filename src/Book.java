147c337fdbed8e8f1eaf9de19b01c63ff7b3cb7b
Mystery135 <websiteemail12345678910@gmail.com>
2023-10-26 10:31:51 -0700

    Initial commit

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Book {
-}
+public class Book { // The fields private String author;
+
+    private String title;
+    private String author;
+
+    /* Set the author and title field when this object is constructed. */
+
+    public Book (String author, String title) {
+
+        this.author = author;
+
+        this.title = title;
+
+// Add the methods here ...
+
+    } }
